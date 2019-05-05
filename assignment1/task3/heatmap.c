2b895ba50a94f8ed3417bac8a6d6e438911836ca
sirexeclp <sirexeclp@gmail.com>
2019-05-05 20:26:07 +0200

    updat_cell function and test

--- assignment1/task3/heatmap.c
+++ assignment1/task3/heatmap.c
@@ -7,19 +7,19 @@
 #define MIN(a,b) ((a) < (b) ? a : b)
 
 typedef struct map{
-    int *cells;
+    double *cells;
     int width;
     int height;
 } map;
 
 int update_cell(map map,int x,int y)
 {
-    int acc = 0;
-    for(int i = MAX(x-1,0); i < MIN(x+1,map.width); i++)
-    {
-        for(int j = MAX(y-1,0); j < MIN(y+1, map.height); j++)
-            acc += map.cells[i,j];
-    }
+    double acc = 0;
+    for(int i = MAX(x-1, 0); i <= MIN(x+1, map.width); i++)
+        for(int j = MAX(y-1, 0); j <= MIN(y+1, map.height); j++)
+            acc += map.cells[i+j*map.width];
+    
+    map.cells[x+y*map.width] = acc / 9.;
 }
 
 int main(int argc, char* argv[])
@@ -29,7 +29,17 @@ int main(int argc, char* argv[])
         printf("usage: ./parsum <threads> <start> <end>");
         return EXIT_FAILURE;
     }
-    int c = [1,2,3,1,2,3,1,2,3];
+    double c[] = {1,2,3
+            ,1,0,1
+            ,1,2,3};
     
+    map map;
+    map.cells = c;
+    map.width=3;
+    map.height =3;
 
+    update_cell(map, 0,0);
+    printf("%f",c[0]);
+
+    return EXIT_SUCCESS;
  }
