f982d319ccde978a38a0f3958e2caf9267d0383b
Paul <Paul.Geppert@Student.HPI.de>
2019-05-09 19:31:13 +0200

    Consistent layout in cpp file

--- assignment1/task3/heatmap.cpp
+++ assignment1/task3/heatmap.cpp
@@ -11,7 +11,7 @@
 
 #define OUTPUT_NAME "output.txt"
 
-class Map{
+class Map {
     public:
         double *cells;
         int width;
@@ -19,14 +19,17 @@ class Map{
 
         Map(int width, int height)
         {
-            this->height=height;
-            this->width=width;
-            this->cells = new double [height*width];
-            for(int j = 0; j < height; j++)
+            this->height = height;
+            this->width = width;
+            this->cells = new double [height * width];
+
+            for (int j = 0; j < height; j++)
                 for (int i = 0; i < width; i++)
-                    cells[j*width+i] = 0;
-            //std::cout << "created map with size:" << width << "x" << height << "\n";
+                {
+                    cells[j * width + i] = 0;
+                }
         }
+
         virtual ~Map()
         {
             delete cells;
@@ -35,27 +38,31 @@ class Map{
         void print()
         {
             std::ofstream output(OUTPUT_NAME);
-            for(int j = 0; j < height; j++) {
+            for (int j = 0; j < height; j++) {
                 for (int i = 0; i < width; i++) {
-                    auto value = cells[j*width+i];
+                    auto value = cells[j * width + i];
                     if (value > .9)
+                    {
                         output << "X";
+                    }
                     else
                     {
-                        value +=0.09;
+                        value += 0.09;
                         value *= 10;
-                        output << ((int)value)%10;
+                        output << ((int) value) % 10;
                     }
                 }
                 output << "\n";
             }
         }
+
         void print(std::string coordinateFile)
         {
             std::ifstream file(coordinateFile);
             std::ofstream output(OUTPUT_NAME);
             std::string line;
-            //throw away the header
+
+            // throw away the header
             std::getline(file, line);
 
             while(std::getline(file, line))
@@ -69,20 +76,21 @@ class Map{
 
         }
 };
+
 struct Hotspot
 {
     int x;
     int y;
     int start_round;
     int end_round;
+
     Hotspot(int x, int y, int start, int end)
     {
         this->x = x;
         this->y = y;
-        start_round = start;
-        end_round = end;
+        this->start_round = start;
+        this->end_round = end;
     }
-
 };
 
 struct WorkPack
@@ -92,22 +100,28 @@ struct WorkPack
 };
 
 
-void update_cell(Map &map,int x,int y)
+void update_cell(Map &map, int x, int y)
 {
     double acc = 0;
-    for(int i = std::max(x-1, 0); i < std::min(x+2, map.width); i++)
-        for(int j = std::max(y-1, 0); j < std::min(y+2, map.height); j++)
+    for (int i = std::max(x-1, 0); i < std::min(x+2, map.width); i++)
+    {
+        for (int j = std::max(y-1, 0); j < std::min(y+2, map.height); j++)
+        {
             acc += map.cells[j*map.width + i];
+        }
+    }
 
     map.cells[x + y * map.width] = acc / 9.;
 }
+
 void update_row(Map &map, int y)
 {
-    for(int x = 0; x < map.width; x++ )
+    for (int x = 0; x < map.width; x++)
     {
         update_cell(map, x, y);
     }
 }
+
 void *run_thread(void * data)
 {
     WorkPack *wp = (WorkPack*) data;
@@ -120,10 +134,11 @@ std::vector<Hotspot> read_input(std::string file_name)
     std::ifstream file(file_name);
     std::vector<Hotspot> hotspots;
     std::string line;
-    //throw away the header
+
+    // throw away the header
     std::getline(file, line);
 
-    while(std::getline(file, line))
+    while (std::getline(file, line))
     {
         std::replace(line.begin(), line.end(), ',', ' ');
         int x, y, start, end;
@@ -131,12 +146,13 @@ std::vector<Hotspot> read_input(std::string file_name)
         lineStream >> x >> y >> start >> end;
         hotspots.push_back(Hotspot(x,y,start,end));
     }
+
     return hotspots;
 }
 
 int main(int argc, char* argv[])
 {
-    if(argc<5)
+    if (argc < 5)
     {
         printf("usage: ./heatmap width height rounds input-file [coordinates-file]");
         return EXIT_FAILURE;
@@ -147,44 +163,51 @@ int main(int argc, char* argv[])
     char * input_file = argv[4];
 
     auto hotSpots = read_input(input_file);
-/*    for(auto item: hotSpots)
-    {
-        std::cout << "x "<< item.x << " y " << item.y  << " start "<< item.start_round <<" end "<< item.end_round << "\n";
-    }*/
 
     int numThreads = map.height;
     pthread_t *threads = new pthread_t[numThreads];
     WorkPack *wp = new WorkPack[numThreads];
 
-    for(auto hotSpot: hotSpots)
-        if(hotSpot.start_round == 0)
-            map.cells[hotSpot.y * map.width + hotSpot.x] = 1;
-
-    for(int round=0; round < rounds; round++)
+    for (auto hotSpot: hotSpots)
     {
-        for(int j = 0; j< map.height; j++)
+        if (hotSpot.start_round == 0)
+        {
+            map.cells[hotSpot.y * map.width + hotSpot.x] = 1;
+        }
+    }
+
+    for (int round = 0; round < rounds; round++)
+    {
+        for (int j = 0; j < map.height; j++)
         {
             wp[j].map = &map;
             wp[j].row = j;
-            pthread_create(&threads[j], NULL, &run_thread,(void*)&wp[j]);
+            pthread_create(&threads[j], NULL, &run_thread, (void*) &wp[j]);
         }
 
-        for(int j = 0; j< map.height; j++)
+        for (int j = 0; j< map.height; j++)
         {
-            pthread_join(threads[j],NULL);
+            pthread_join(threads[j], NULL);
         }
 
-        for(auto hotSpot: hotSpots)
-            if((round >= hotSpot.start_round) && (round < hotSpot.end_round ))
+        for (auto hotSpot: hotSpots)
+        {
+            if ((round >= hotSpot.start_round) && (round < hotSpot.end_round))
+            {
                 map.cells[hotSpot.y * map.width + hotSpot.x] = 1;
+            }
+        }
     }
 
-    if(argc == 6)
+    if (argc == 6)
     {
         auto coordinateFile = argv[5];
         map.print(coordinateFile);
     }
     else
+    {
         map.print();
+    }
+
     return EXIT_SUCCESS;
  }
