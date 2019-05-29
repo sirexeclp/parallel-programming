a61b3dce1a3a643f147844005a3f1f9e1fdde393
Paul Geppert <Paul.Geppert@Student.HPI.de>
2019-05-29 22:23:41 +0200

    Effectively ignore target folder, added input files, load files into memory

--- assignment2/task4/src/main/java/Pargrepmon.java
+++ assignment2/task4/src/main/java/Pargrepmon.java
@@ -1,5 +1,10 @@
-import java.util.LinkedList;
+import java.io.IOException;
+import java.nio.charset.Charset;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
+import java.util.ArrayList;
 
 public class Pargrepmon {
     public static void main (String[] args) {
@@ -10,7 +15,20 @@ public class Pargrepmon {
         String searchStringFilePath = args[0];
         String inputDataFilePath = args[1];
 
-        List<String> searchStrings = new LinkedList<String>();
-        List<String> inputLines = new LinkedList<String>();
+        Path searchFile = Paths.get(searchStringFilePath);
+        List<String> searchStrings = new ArrayList<>();
+        try {
+            searchStrings = Files.readAllLines(searchFile, Charset.defaultCharset());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Path inputFile = Paths.get(inputDataFilePath);
+        List<String> lines = new ArrayList<>();
+        try {
+            lines = Files.readAllLines(inputFile, Charset.defaultCharset());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
