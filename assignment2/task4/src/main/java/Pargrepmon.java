f0237a1b036c72a1d98e95913bfbea3a952375d4
Paul Geppert <Paul.Geppert@Student.HPI.de>
2019-05-29 22:00:57 +0200

    Parse input arguments

--- assignment2/task4/src/main/java/Pargrepmon.java
+++ assignment2/task4/src/main/java/Pargrepmon.java
@@ -1,6 +1,16 @@
+import java.util.LinkedList;
+import java.util.List;
+
 public class Pargrepmon {
-    public static void main (String[] args)
-    {
-        System.out.println("Hello World!");
+    public static void main (String[] args) {
+        if (args.length != 2) {
+            System.err.println("[Error] Usage: <program> <search string file path> <input data file path>");
+        }
+
+        String searchStringFilePath = args[0];
+        String inputDataFilePath = args[1];
+
+        List<String> searchStrings = new LinkedList<String>();
+        List<String> inputLines = new LinkedList<String>();
     }
 }
