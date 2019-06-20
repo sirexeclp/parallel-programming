18bb2e7363a7835b57ad62fc5494e551035bd17d
felix <sirexeclp@gmail.com>
2019-06-20 20:15:29 +0200

    es wird langsam

--- assignment3/task1/parsum.cpp
+++ assignment3/task1/parsum.cpp
@@ -140,6 +140,84 @@ static int print_u128_u(uint128_t u128) {
     return rc;
 }
 
+const char *getErrorString(cl_int error)
+{
+switch(error){
+    // run-time and JIT compiler errors
+    case 0: return "CL_SUCCESS";
+    case -1: return "CL_DEVICE_NOT_FOUND";
+    case -2: return "CL_DEVICE_NOT_AVAILABLE";
+    case -3: return "CL_COMPILER_NOT_AVAILABLE";
+    case -4: return "CL_MEM_OBJECT_ALLOCATION_FAILURE";
+    case -5: return "CL_OUT_OF_RESOURCES";
+    case -6: return "CL_OUT_OF_HOST_MEMORY";
+    case -7: return "CL_PROFILING_INFO_NOT_AVAILABLE";
+    case -8: return "CL_MEM_COPY_OVERLAP";
+    case -9: return "CL_IMAGE_FORMAT_MISMATCH";
+    case -10: return "CL_IMAGE_FORMAT_NOT_SUPPORTED";
+    case -11: return "CL_BUILD_PROGRAM_FAILURE";
+    case -12: return "CL_MAP_FAILURE";
+    case -13: return "CL_MISALIGNED_SUB_BUFFER_OFFSET";
+    case -14: return "CL_EXEC_STATUS_ERROR_FOR_EVENTS_IN_WAIT_LIST";
+    case -15: return "CL_COMPILE_PROGRAM_FAILURE";
+    case -16: return "CL_LINKER_NOT_AVAILABLE";
+    case -17: return "CL_LINK_PROGRAM_FAILURE";
+    case -18: return "CL_DEVICE_PARTITION_FAILED";
+    case -19: return "CL_KERNEL_ARG_INFO_NOT_AVAILABLE";
+
+    // compile-time errors
+    case -30: return "CL_INVALID_VALUE";
+    case -31: return "CL_INVALID_DEVICE_TYPE";
+    case -32: return "CL_INVALID_PLATFORM";
+    case -33: return "CL_INVALID_DEVICE";
+    case -34: return "CL_INVALID_CONTEXT";
+    case -35: return "CL_INVALID_QUEUE_PROPERTIES";
+    case -36: return "CL_INVALID_COMMAND_QUEUE";
+    case -37: return "CL_INVALID_HOST_PTR";
+    case -38: return "CL_INVALID_MEM_OBJECT";
+    case -39: return "CL_INVALID_IMAGE_FORMAT_DESCRIPTOR";
+    case -40: return "CL_INVALID_IMAGE_SIZE";
+    case -41: return "CL_INVALID_SAMPLER";
+    case -42: return "CL_INVALID_BINARY";
+    case -43: return "CL_INVALID_BUILD_OPTIONS";
+    case -44: return "CL_INVALID_PROGRAM";
+    case -45: return "CL_INVALID_PROGRAM_EXECUTABLE";
+    case -46: return "CL_INVALID_KERNEL_NAME";
+    case -47: return "CL_INVALID_KERNEL_DEFINITION";
+    case -48: return "CL_INVALID_KERNEL";
+    case -49: return "CL_INVALID_ARG_INDEX";
+    case -50: return "CL_INVALID_ARG_VALUE";
+    case -51: return "CL_INVALID_ARG_SIZE";
+    case -52: return "CL_INVALID_KERNEL_ARGS";
+    case -53: return "CL_INVALID_WORK_DIMENSION";
+    case -54: return "CL_INVALID_WORK_GROUP_SIZE";
+    case -55: return "CL_INVALID_WORK_ITEM_SIZE";
+    case -56: return "CL_INVALID_GLOBAL_OFFSET";
+    case -57: return "CL_INVALID_EVENT_WAIT_LIST";
+    case -58: return "CL_INVALID_EVENT";
+    case -59: return "CL_INVALID_OPERATION";
+    case -60: return "CL_INVALID_GL_OBJECT";
+    case -61: return "CL_INVALID_BUFFER_SIZE";
+    case -62: return "CL_INVALID_MIP_LEVEL";
+    case -63: return "CL_INVALID_GLOBAL_WORK_SIZE";
+    case -64: return "CL_INVALID_PROPERTY";
+    case -65: return "CL_INVALID_IMAGE_DESCRIPTOR";
+    case -66: return "CL_INVALID_COMPILER_OPTIONS";
+    case -67: return "CL_INVALID_LINKER_OPTIONS";
+    case -68: return "CL_INVALID_DEVICE_PARTITION_COUNT";
+
+    // extension errors
+    case -1000: return "CL_INVALID_GL_SHAREGROUP_REFERENCE_KHR";
+    case -1001: return "CL_PLATFORM_NOT_FOUND_KHR";
+    case -1002: return "CL_INVALID_D3D10_DEVICE_KHR";
+    case -1003: return "CL_INVALID_D3D10_RESOURCE_KHR";
+    case -1004: return "CL_D3D10_RESOURCE_ALREADY_ACQUIRED_KHR";
+    case -1005: return "CL_D3D10_RESOURCE_NOT_ACQUIRED_KHR";
+    default: return "Unknown OpenCL error";
+    }
+}
+
+
 int main(int argc, char *argv[]) {
     if (argc < 3) {
         printf("usage: ./parsum <start> <end>");
@@ -199,37 +277,20 @@ int main(int argc, char *argv[]) {
 
 		// // // std::vector<double> c(N);
 
-		u_int64_t max_n = selectedDevice.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>();
+		u_int64_t max_n = 250;//selectedDevice.getInfo<CL_DEVICE_MAX_WORK_GROUP_SIZE>();
 		auto dims =  selectedDevice.getInfo<CL_DEVICE_MAX_WORK_ITEM_SIZES>();
 		
 
-		u_int16_t num_workpacks = std::ceil(range / float(max_n));
+		u_int16_t num_workpacks = std::ceil((range +1)/ float(max_n));
 
 		std::cout << "num_workpacks: " << num_workpacks << "\n";
 		// // // // Allocate device buffers and transfer input data to device.
 		// // // cl::Buffer A(context, a.begin(),a.end(), false);
+		// auto prefSize = kernel.getInfo<CL_KERNEL_WORK_GROUP_SIZE>();
+		// std::cout << prefSize[0] <<"\n";
 
-
-
-		std::vector<uint64_t> workpack_start(num_workpacks);
-		std::vector<uint64_t> workpack_end(num_workpacks);
-		
-		for(int i = 0; i<num_workpacks; i++)
-		{
-			workpack_start[i] = start + (i*max_n);
-			workpack_end[i] = std::min(workpack_start[i] + max_n, end);
-			std::cout << workpack_start[i] << " " << workpack_end[i] << "\n";
-		}
-
-		cl::Buffer b_workpack_start(context, CL_MEM_READ_WRITE, sizeof( uint64_t) * num_workpacks);
-		cl::Buffer b_workpack_end(context, CL_MEM_READ_WRITE, sizeof( uint64_t) * num_workpacks);
-		cl::Buffer b_workpack_result(context, CL_MEM_READ_WRITE, sizeof( uint64_t) *(num_workpacks +2));
+		cl::Buffer b_workpack_result(context, CL_MEM_READ_WRITE, sizeof( uint64_t) *(num_workpacks ));
 		// // // GenericBuffer<double> C(context, c, false);
-
-
-
-		queue.enqueueWriteBuffer(b_workpack_start,CL_TRUE,0,sizeof(uint64_t) * workpack_start.size() ,workpack_start.data());
-		queue.enqueueWriteBuffer(b_workpack_end,CL_TRUE,0,sizeof(uint64_t) * workpack_end.size() ,workpack_end.data());
 
 
 		// kernel.setArg(0, start);
@@ -242,34 +303,38 @@ int main(int argc, char *argv[]) {
 		// kernel.setArg(4, A);
 		// kernel.setArg(5, B);
 
-		std::vector<uint64_t> results;
+		// std::vector<uint64_t> results;
 
-		for(int workpackId = 0; workpackId<num_workpacks; workpackId++)
-		{
+		// for(int workpackId = 0; workpackId<num_workpacks; workpackId++)
+		// {
 
-			u_int64_t threads = std::min(workpack_end[workpackId]- workpack_start[workpackId]+1,max_n);
+			u_int64_t threads =  range +1;//std::min(workpack_end[workpackId]- workpack_start[workpackId]+1,max_n);
 
-			u_int64_t x = std::min(threads, dims[0]);
-			u_int64_t y = std::min(threads/x, dims[1]);
-			u_int64_t z = std::min(threads/(y*x), dims[2]);
+			u_int64_t x = threads % UINT16_MAX;//std::min(threads, dims[0]);
+			u_int64_t y = threads / UINT16_MAX+1;//std::min(threads/x, dims[1]);
+			u_int64_t z = 1;//std::min(threads/(y*x), dims[2]);
 			std::cout << "optimal work item sizes\nx: " << x << "\ny: " << y << "\nz: " << z << "\n"; 
 
+			size_t start_x = start % UINT16_MAX;
+			size_t start_y = start / UINT16_MAX;
 
-			kernel.setArg(0, (uint64_t) workpackId);
-			kernel.setArg(1, b_workpack_start);
-			kernel.setArg(2, b_workpack_end);
-			kernel.setArg(3, b_workpack_result);
-			kernel.setArg(4, (num_workpacks +2) * sizeof(uint64_t), NULL);
+			// kernel.setArg(0, (uint64_t) workpackId);
+			// kernel.setArg(1, b_workpack_start);
+			// kernel.setArg(2, b_workpack_end);
+			kernel.setArg(0, b_workpack_result);
+			kernel.setArg(1, (num_workpacks ) * sizeof(uint64_t), NULL);
 
 			// queue.enqueueNDRangeKernel(kernel, x, y , z);
-			queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(x,y,z) , cl::NullRange);
-			uint64_t result;
-			queue.enqueueReadBuffer(b_workpack_result, CL_TRUE, 0, sizeof(result), &result);
-			results.push_back(result);
-		}
+			std::cout << "start:"<< (start_y << 32) + start_x << "\n";
+			queue.enqueueNDRangeKernel(kernel, cl::NDRange(start_x,start_y,0) , cl::NDRange(x,y,z) , cl::NullRange);
+			std::vector<uint64_t> result(num_workpacks);
+			queue.enqueueReadBuffer(b_workpack_result, CL_TRUE, 0, sizeof(uint64_t) * num_workpacks, result.data());
+			//results.push_back(result);
+			std::cout << result[0] << "\n";
+		// }
 
 		uint64_t total;
-		for(auto i: results)
+		for(auto i: result)
 		{
 			total +=i;
 		}
@@ -307,7 +372,7 @@ int main(int argc, char *argv[]) {
 	{
 		std::cerr
 			<< "OpenCL error: "
-			<< err.what() << "(" << err.err() << ")"
+			<< err.what() << "(" << getErrorString(err.err()) << ")"
 			<< std::endl;
 		return 1;
 	}
